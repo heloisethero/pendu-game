37ecca95034342adba9a2c954831c9ddba1264c1
Heloise Thero <thero.heloise@gmail.com>
2020-05-28 11:12:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const POSSIBLE_WORDS = [
+  "BALLAST", 
+  "CITHARE", 
+  "QUETSCHE", 
+  "TOMAHAWK", 
+  "BALALAIKA", 
+  "QUEMANDER",
+  "FLIBUSTIER",
+  "MAHARADJAH"
+]
+const LETTERS = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+const HIDDEN_SYMBOL = "_"
+
+const DEFAULT_STATE = {
+  guesses: 0,
+  lettersAlreadyUsed: [],
+}
+
+const WordToGuess = ({ wordToGuess, lettersAlreadyUsed }) => (
+  <div className="wordToGuess">
+    {[...wordToGuess].map(
+      (letter, index) =>(
+      <span key={index}>
+        {lettersAlreadyUsed.includes(letter) ? letter : HIDDEN_SYMBOL} </span>
+    ))}
+  </div>
+)
+
+const GuessCount = ({ guesses }) => (
+  <div className="guesses">Number of guesses: {guesses}</div>
+)
+
+const LetterButton = ({ letter, alreadyClicked, onClick }) => (
+  <div className={`letterButton ${alreadyClicked}`} onClick={() => onClick(letter)}>
+    <span className="letter">
+      {letter}
+    </span>
+  </div>
+  )
+
+class App extends Component {
+  state = {
+    ...DEFAULT_STATE,
+    wordToGuess: this.generateWordToGuess(),
+  }
+
+  generateWordToGuess(){
+    return POSSIBLE_WORDS[Math.floor(Math.random() * POSSIBLE_WORDS.length)]
+  }
+
+  handleLetterClick = letter => {
+    this.setState({ 
+      guesses: this.state.guesses + 1, 
+      lettersAlreadyUsed: [...this.state.lettersAlreadyUsed, letter]
+    })
+  }
+
+  resetGame = () => {
+    this.setState({
+      ...DEFAULT_STATE,
+      wordToGuess: this.generateWordToGuess(),
+    })
+  }
+
+  render() {
+    const { wordToGuess, guesses, lettersAlreadyUsed } = this.state
+
+    return (
+      <div className="pendu">
+        <WordToGuess wordToGuess={wordToGuess} lettersAlreadyUsed={lettersAlreadyUsed} />
+        {LETTERS.map(letter => (
+            <LetterButton 
+              letter={letter}
+              onClick={this.handleLetterClick}
+              alreadyClicked={lettersAlreadyUsed.includes(letter)}
+              key={letter}
+            />
+          ))}
+          <GuessCount guesses={guesses} />
+      </div>
+    )
+  }
 }
 
 export default App;
