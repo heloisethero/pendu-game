5c308d5e2b4b503fd8fe3ffc18504402a7047023
heloisethero <thero.heloise@gmail.com>
2020-06-18 00:38:58 +0200

    Updates

--- precache-manifest.d06957f7c09fcfb9b7c7404ca83bbf39.js
+++ precache-manifest.d06957f7c09fcfb9b7c7404ca83bbf39.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0f4d16b37be4ba37eb1d8b82390cb8a0",
+    "revision": "11fd0d76883746a649802ff530e3daa5",
     "url": "/pendu-game/index.html"
   },
   {
-    "revision": "526b5b446076c51308e1",
-    "url": "/pendu-game/static/css/main.f42d911e.chunk.css"
+    "revision": "edd71a788b4ce0ef934b",
+    "url": "/pendu-game/static/css/main.238cc3a4.chunk.css"
   },
   {
     "revision": "4120e5c849a0c494bc0c",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pendu-game/static/js/2.5f9a8d7f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "526b5b446076c51308e1",
+    "revision": "edd71a788b4ce0ef934b",
     "url": "/pendu-game/static/js/main.3c3cc02c.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pendu-game/precache-manifest.417df4c4890137a8995fbe107527eb7d.js"
+  "/pendu-game/precache-manifest.d06957f7c09fcfb9b7c7404ca83bbf39.js"
 );
 
 self.addEventListener('message', (event) => {
