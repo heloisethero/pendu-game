fe8f021876cc4205387f3d6f9d00c9bd42e9442b
heloisethero <thero.heloise@gmail.com>
2020-06-17 18:23:06 +0200

    Updates

---
+++ precache-manifest.f153851a4fea0ea730f732d2be7f4c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe9f15718589de58304ef66c086979d",
+    "url": "/pendu-game/index.html"
+  },
+  {
+    "revision": "537d4e3495e6c2204680",
+    "url": "/pendu-game/static/css/main.9eda47e4.chunk.css"
+  },
+  {
+    "revision": "4120e5c849a0c494bc0c",
+    "url": "/pendu-game/static/js/2.5f9a8d7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pendu-game/static/js/2.5f9a8d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537d4e3495e6c2204680",
+    "url": "/pendu-game/static/js/main.19bcf992.chunk.js"
+  },
+  {
+    "revision": "4825ab918ec9c6f7dff8",
+    "url": "/pendu-game/static/js/runtime-main.4b73a932.js"
+  }
+]);
